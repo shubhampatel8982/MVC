019fe064b82348f48081740f9ff5db2ab61a1c2e
shubhampatel8982 <155613423+shubhampatel8982@users.noreply.github.com>
2024-08-21 16:25:57 +0530

    Add files via upload

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_ASSIGNMENT.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
